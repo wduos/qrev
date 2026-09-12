5e568630e3c1f3183d57123ece00ba469323b975
Wendel <wendel.duarte@proton.me>
2026-09-12 11:17:57 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1 className="title">QRev</h1>
+        <hr />
+        <button className="main-btn" type="button">
+          Criar Novo
+        </button>
+      </header>
+    </>
+  );
+}
+
+export default App;
